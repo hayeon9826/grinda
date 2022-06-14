4f7636d8c691c4f5d9b0977d7d274e4715db7607
hayeon9826 <hayeonkim226@gmail.com>
2022-06-14 23:07:24 +0900

    edit database url

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,12 @@
 module.exports = {
-  mode: "jit",
-  purge: ["./pages/**/*.{js,ts,jsx,tsx}", "./components/**/*.{js,ts,jsx,tsx}"],
-  darkMode: false, // or 'media' or 'class'
-  theme: {
-    extend: {},
-  },
-  plugins: [require("@tailwindcss/forms")],
+    mode: "jit",
+    purge: [
+        "./pages/**/*.{js,ts,jsx,tsx}",
+        "./components/**/*.{js,ts,jsx,tsx}",
+    ],
+    media: false, // or 'media' or 'class'
+    theme: {
+        extend: {},
+    },
+    plugins: [require("@tailwindcss/forms")],
 };
